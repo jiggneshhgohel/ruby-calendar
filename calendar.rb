b44b8fbf00342b7985dc057842923715894f9b0b
Jignesh Gohel <jignesh.ghl@gmail.com>
2014-11-28 12:23:09 +0530

    Added following failing specs FIRST
        context "#week_day_name_today" do
            it "returns today's week day name" do
                calendar = Calendar.new
                expect(calendar.week_day_name_today).to eql(Time.now.strftime('%A'))
            end
        end
    
        context "#week_day_number_today" do
            it "returns today's week day number" do
                calendar = Calendar.new
                expect(calendar.week_day_number_today).to eql(Time.now.wday)
            end
        end
    
    Then added the simplest implementation to pass the above failing specs in Calendar class

--- calendar.rb
+++ calendar.rb
@@ -2,4 +2,12 @@ class Calendar
     def date_today
         Time.now.day
     end
+
+    def week_day_name_today
+        Time.now.strftime('%A')
+    end
+
+    def week_day_number_today
+        Time.now.wday
+    end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -7,4 +7,20 @@ describe Calendar do
             expect(calendar.date_today).to eql(Time.now.day)
         end
     end
+
+    context "#week_day_name_today" do
+        it "returns today's week day name" do
+            calendar = Calendar.new
+            expect(calendar.week_day_name_today).to eql(Time.now.strftime('%A'))
+        end
+    end
+
+    context "#week_day_number_today" do
+        it "returns today's week day number" do
+            calendar = Calendar.new
+            expect(calendar.week_day_number_today).to eql(Time.now.wday)
+        end
+    end
+
+    # today's week day number
 end
