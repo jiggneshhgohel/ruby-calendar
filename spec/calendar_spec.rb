fa30b8f053f52f80912e86ac6ccb903f860d13ab
Jignesh Gohel <jignesh.ghl@gmail.com>
2014-11-28 11:50:05 +0530

    1. Added empty spec spec/calendar_spec.rb for a top-level class Calendar defined in calendar.rb representing the Calendar
    2. Added .rspec file in the root of the project. RSpec conventionally reads the command line configuration options supported by it from this file for given project.
    
    Reference: https://www.relishapp.com/rspec/rspec-core/v/2-4/docs/configuration/read-command-line-configuration-options-from-files

---
+++ spec/calendar_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../calendar'
+
+describe Calendar do
+    context "#for_ongoing_month" do
+        it "returns the calendar of ongoing month" do
+            skip "TODO"
+        end
+    end
+end
