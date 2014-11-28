9437d3333b2068fdd9fe896957dce7c183a0e6fe
Jignesh Gohel <jignesh.ghl@gmail.com>
2014-11-28 18:19:41 +0530

    Described the desired Calendar API in spec and added empty methods in Calendar class.

--- calendar.rb
+++ calendar.rb
@@ -1,3 +1,21 @@
 class Calendar
+    private_class_method :new
 
+    class << self
+        def for_year(year)
+            #TODO
+        end
+
+        alias_method :in_year, :for_year
+
+        def for_month(month)
+            #TODO
+        end
+
+        alias_method :of_month, :for_month
+
+        def for_week(week)
+            #TODO
+        end
+    end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,4 +1,32 @@
 require_relative '../calendar'
 
+# Desired Calendar API:
+#   Calendar.for_week(4).of_month(8).in_year(2012).show # Weekly view
+#   Calendar.for_month(6).in_year(2011).show            # Monthly view
+#   Calendar.for_year(2014).show                        # Yearly view
+
 describe Calendar do
+    it "does not respond to class method new" do
+        expect(Calendar).not_to respond_to(:new)
+    end
+
+    it "responds to class method for_week" do
+        expect(Calendar).to respond_to(:for_week)
+    end
+
+    it "responds to class method of_month" do
+        expect(Calendar).to respond_to(:of_month)
+    end
+
+    it "responds to class method for_month" do
+        expect(Calendar).to respond_to(:for_month)
+    end
+
+    it "responds to class method in_year" do
+        expect(Calendar).to respond_to(:in_year)
+    end
+
+    it "responds to class method for_year" do
+        expect(Calendar).to respond_to(:for_year)
+    end
 end
