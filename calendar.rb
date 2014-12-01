614c22abf4dfbe762c0490b896c3be2d0dc04256
Jignesh Gohel <jignesh.ghl@gmail.com>
2014-12-01 16:31:37 +0530

    Added implementation for supporting following Calendar API and their specs:
    * Calendar.for_week(4).of_month(8).in_year(2012)
    * Calendar.for_month(6).in_year(2011)
    * Calendar.for_year(2014)
    * Calendar.of_month(5)
    * Calendar.for_current_week
    * Calendar.for_current_month
    * Calendar.for_current_year
    
    Handled following invalid usages:
    
    * Calendar.for_week(4).show
    * Calendar.for_month(7).show
    
    Note: The show implementation is not complete yet and more specs are required to be added esp the main implementation for generating the calendar is pending.

--- calendar.rb
+++ calendar.rb
@@ -1,21 +1,76 @@
 class Calendar
     private_class_method :new
 
+    attr_reader :year, :month, :week
+
+    def initialize(year, month=nil, week=nil)
+        @year = year
+        @month = month
+        @week = week
+    end
+
+    def in_year(year)
+        @year = year
+        self
+    end
+
+    def of_month(month)
+        @month = month
+        self
+    end
+
+    def show
+        validate_object_state
+    end
+
     class << self
         def for_year(year)
-            #TODO
+            new(year)
         end
-
-        alias_method :in_year, :for_year
 
         def for_month(month)
-            #TODO
+            new(nil, month)
         end
 
-        alias_method :of_month, :for_month
+        def of_month(month)
+            new(Time.now.year, month)
+        end
 
         def for_week(week)
-            #TODO
+            new(nil, nil, week)
+        end
+
+        def for_current_week
+            time = current_time
+            #  Week number of the year. The week starts with Monday. (00..53)
+            new(time.year, time.month, time.strftime("%W").to_i)
+        end
+
+        def for_current_month
+            time = current_time
+            new(time.year, time.month)
+        end
+
+        def for_current_year
+            new(current_time.year)
+        end
+
+        private
+
+        def current_time
+            Time.now
+        end
+    end
+
+    private
+
+    def validate_object_state
+        if month && !year
+            raise "Incorrect API Usage.Required year component missing."
+        end
+
+        if week && !(year && month)
+            raise "Incorrect API Usage.Required month and year components missing."
         end
     end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -4,6 +4,15 @@ require_relative '../calendar'
 #   Calendar.for_week(4).of_month(8).in_year(2012).show # Weekly view
 #   Calendar.for_month(6).in_year(2011).show            # Monthly view
 #   Calendar.for_year(2014).show                        # Yearly view
+#   Calendar.of_month(5).show                           # Monthly view (current years' month calendar)
+
+#   Calendar.for_current_week.show                      # Weekly view
+#   Calendar.for_current_month.show                     # Monthly View
+#   Calendar.for_current_year.show                      # Yearly view
+
+#   Calendar.for_week(4).show                           # Incorrect.Please set month and year
+#   Calendar.for_month(7).show                          # Incorrect.Please set year or use of_month
+#
 
 describe Calendar do
     it "does not respond to class method new" do
@@ -14,19 +23,176 @@ describe Calendar do
         expect(Calendar).to respond_to(:for_week)
     end
 
-    it "responds to class method of_month" do
-        expect(Calendar).to respond_to(:of_month)
-    end
-
     it "responds to class method for_month" do
         expect(Calendar).to respond_to(:for_month)
-    end
-
-    it "responds to class method in_year" do
-        expect(Calendar).to respond_to(:in_year)
     end
 
     it "responds to class method for_year" do
         expect(Calendar).to respond_to(:for_year)
     end
+
+    it "responds to instance method of_month" do
+        expect(Calendar.for_week(2)).to respond_to(:of_month)
+    end
+
+    it "responds to instance method in_year" do
+        expect(Calendar.for_week(2)).to respond_to(:in_year)
+    end
+
+    context ".for_year" do
+        let!(:calendar) { Calendar.for_year(2011) }
+
+        it "sets the year to received year" do
+            expect(calendar.year).to eql(2011)
+        end
+
+        it "sets the month to nil" do
+            expect(calendar.month).to be_nil
+        end
+
+        it "sets the week to nil" do
+            expect(calendar.week).to be_nil
+        end
+    end
+
+    context ".for_month.in_year" do
+        let!(:calendar) { Calendar.for_month(2).in_year(2012) }
+
+        it "responds to instance method in_year" do
+            expect(calendar).to respond_to(:in_year)
+        end
+
+        it "sets the year to received year" do
+            expect(calendar.year).to eql(2012)
+        end
+
+        it "sets the month to received month" do
+            expect(calendar.month).to eql(2)
+        end
+
+        it "sets the week to nil" do
+            expect(calendar.week).to be_nil
+        end
+    end
+
+    context ".for_week.of_month.in_year" do
+        let!(:calendar) { Calendar.for_week(2).of_month(4).in_year(2014) }
+
+        it "responds to instance method of_month" do
+            expect(calendar).to respond_to(:of_month)
+        end
+
+        it "responds to instance method in_year" do
+            expect(calendar).to respond_to(:in_year)
+        end
+
+        it "sets the year to received year" do
+            expect(calendar.year).to eql(2014)
+        end
+
+        it "sets the month to received month" do
+            expect(calendar.month).to eql(4)
+        end
+
+        it "sets the week to received week" do
+            expect(calendar.week).to eql(2)
+        end
+    end
+
+    context ".of_month" do
+        let!(:calendar) { Calendar.of_month(8) }
+
+        it "responds to class method of_month" do
+            expect(Calendar).to respond_to(:of_month)
+        end
+
+        it "sets the month to received month" do
+            expect(calendar.month).to eql(8)
+        end
+
+        it "sets the year to current year" do
+            expect(calendar.year).to eql(Time.now.year)
+        end
+    end
+
+    context ".for_current_week" do
+        let!(:calendar) { Calendar.for_current_week }
+
+        it "it responds to class method for_current_week" do
+            expect(Calendar).to respond_to(:for_current_week)
+        end
+
+        it "sets the year to current year" do
+            expect(calendar.year).to eql(Time.now.year)
+        end
+
+        it "sets the month to current month" do
+            expect(calendar.month).to eql(Time.now.month)
+        end
+
+        it "sets the week to current week number in year" do
+            expect(calendar.week).to eql(Time.now.strftime("%W").to_i)
+        end
+    end
+
+    context ".for_current_month" do
+        let!(:calendar) { Calendar.for_current_month }
+
+        it "it responds to class method for_current_month" do
+            expect(Calendar).to respond_to(:for_current_month)
+        end
+
+        it "sets the year to current year" do
+            expect(calendar.year).to eql(Time.now.year)
+        end
+
+        it "sets the month to current month" do
+            expect(calendar.month).to eql(Time.now.month)
+        end
+
+        it "does not set the week" do
+            expect(calendar.week).to be_nil
+        end
+    end
+
+    context ".for_current_year" do
+        let!(:calendar) { Calendar.for_current_year }
+
+        it "it responds to class method for_current_year" do
+            expect(Calendar).to respond_to(:for_current_year)
+        end
+
+        it "sets the year to current year" do
+            expect(calendar.year).to eql(Time.now.year)
+        end
+
+        it "does not set the month" do
+            expect(calendar.month).to be_nil
+        end
+
+        it "does not set the week" do
+            expect(calendar.week).to be_nil
+        end
+    end
+
+    context "#show" do
+        it "responds to instance method show" do
+            expect(Calendar.for_current_week).to respond_to(:show)
+        end
+
+        it "raises error when week is found set without month and year" do
+            expect { Calendar.for_week(5).show }.to raise_error(/Incorrect API Usage.Required month and year components missing./)
+        end
+
+        it "raises error when month is found set without year" do
+            expect { Calendar.for_month(4).show }.to raise_error(/Incorrect API Usage.Required year component missing./)
+        end
+
+        it "does not raise error when week is not found set but month and year are set" do
+            expect { Calendar.for_month(4).in_year(2011).show }.not_to raise_error
+        end
+
+        # TODO: Add more specs
+    end
+
 end
