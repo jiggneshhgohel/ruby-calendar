af867b87092c9b8a7b6b248ab1720620ca6d4acb
Jignesh Gohel <jignesh.ghl@gmail.com>
2014-12-02 17:28:27 +0530

    Added following class methods and their specs: 	current_week_number_of_current_year 	current_week_number_of_current_month 	week_number_of_month(time=Time.now) 	week_number_of_year(time=Time.now)
    Added constant:
    	MAP_MONTH_NUMBER_TO_MONTH_DETAIL
    
    Organized the specs

--- calendar.rb
+++ calendar.rb
@@ -1,4 +1,19 @@
 class Calendar
+    MAP_MONTH_NUMBER_TO_MONTH_DETAILS = {
+        "1" =>  [ :jan, 31 ],
+        "2" =>  [ :feb, 28 ], # TODO: Leap year days to be handled later
+        "3" =>  [ :mar, 31 ],
+        "4" =>  [ :apr, 30 ],
+        "5" =>  [ :may, 31 ],
+        "6" =>  [ :jun, 30 ],
+        "7" =>  [ :jul, 31 ],
+        "8" =>  [ :aug, 31] ,
+        "9" =>  [ :sep, 30 ],
+        "10" => [ :oct, 31 ],
+        "11" => [ :nov, 30 ],
+        "12" => [ :dec, 31 ]
+    }
+
     private_class_method :new
 
     attr_reader :year, :month, :week
@@ -42,8 +57,7 @@ class Calendar
 
         def for_current_week
             time = current_time
-            #  Week number of the year. The week starts with Monday. (00..53)
-            new(time.year, time.month, time.strftime("%W").to_i)
+            new(time.year, time.month, current_week_number_of_current_year)
         end
 
         def for_current_month
@@ -55,11 +69,51 @@ class Calendar
             new(current_time.year)
         end
 
+        def current_week_number_of_current_year
+            week_number_of_year # by default this method uses current datetime
+        end
+
+        def current_week_number_of_current_month
+            week_number_of_month # by default this method uses current datetime
+        end
+
+        def week_number_of_month(time=Time.now)
+            month_day_number = time.day
+
+            # Adding 1 for handling the special cases like first week dates
+            # whose division by 7 returns 0
+            week_number = (month_day_number / 7) + 1
+
+            if 0 == (month_day_number % 7)
+                week_number -= 1
+            end
+            week_number
+        end
+
+        def week_number_of_year(time=Time.now)
+            year_day_number = time.yday
+
+            # Adding 1 for handling the special cases like first week dates
+            # whose division by 7 returns 0
+            week_number = (year_day_number / 7) + 1
+            if 0 == (year_day_number % 7)
+                week_number -= 1
+            end
+            week_number
+        end
+
         private
 
         def current_time
             Time.now
         end
+
+        # TODO: Expects numeric month number.Later support for downcased abbreviated
+        # month name can be added
+        def number_of_days_in_month(month)
+            MAP_MONTH_NUMBER_TO_MONTH_DETAILS[month.to_s][1]
+        end
+
     end
 
     private
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -15,28 +15,96 @@ require_relative '../calendar'
 #
 
 describe Calendar do
-    it "does not respond to class method new" do
+    it "does not have class method new" do
         expect(Calendar).not_to respond_to(:new)
     end
 
-    it "responds to class method for_week" do
-        expect(Calendar).to respond_to(:for_week)
+    context "has class method" do
+        it "for_week" do
+            expect(Calendar).to respond_to(:for_week)
+        end
+
+        it "for_month" do
+            expect(Calendar).to respond_to(:for_month)
+        end
+
+        it "for_year" do
+            expect(Calendar).to respond_to(:for_year)
+        end
+
+        it "of_month" do
+            expect(Calendar).to respond_to(:of_month)
+        end
+
+        it "for_current_week" do
+            expect(Calendar).to respond_to(:for_current_week)
+        end
+
+        it "for_current_month" do
+            expect(Calendar).to respond_to(:for_current_month)
+        end
+
+        it "for_current_year" do
+            expect(Calendar).to respond_to(:for_current_year)
+        end
+
+        it "week_number_of_month" do
+            expect(Calendar).to respond_to(:week_number_of_month)
+        end
+
+        it "week_number_of_year" do
+            expect(Calendar).to respond_to(:week_number_of_year)
+        end
+
+        it "current_week_number_of_current_year" do
+            expect(Calendar).to respond_to(:current_week_number_of_current_year)
+        end
+
+        it "current_week_number_of_current_month" do
+            expect(Calendar).to respond_to(:current_week_number_of_current_month)
+        end
     end
 
-    it "responds to class method for_month" do
-        expect(Calendar).to respond_to(:for_month)
+    context "has instance method" do
+        it "of_month" do
+            expect(Calendar.for_week(2)).to respond_to(:of_month)
+        end
+
+        it "in_year" do
+            expect(Calendar.for_week(2)).to respond_to(:in_year)
+        end
     end
 
-    it "responds to class method for_year" do
-        expect(Calendar).to respond_to(:for_year)
+    context ".week_number_of_month" do
+        it "returns the week-number of month in which given date lies" do
+            expect(Calendar.week_number_of_month(Time.new(2014, 10, 9))).to eql(2)
+            expect(Calendar.week_number_of_month(Time.new(2014, 10, 1))).to eql(1)
+            expect(Calendar.week_number_of_month(Time.new(2012, 4, 23))).to eql(4)
+            expect(Calendar.week_number_of_month(Time.new(2015, 7, 31))).to eql(5)
+            expect(Calendar.week_number_of_month(Time.new(2014, 12, 7))).to eql(1)
+        end
     end
 
-    it "responds to instance method of_month" do
-        expect(Calendar.for_week(2)).to respond_to(:of_month)
+    context ".week_number_of_year" do
+        it "returns the week-number of year in which given date lies" do
+            expect(Calendar.week_number_of_year(Time.new(2014, 1, 1))).to eql(1)
+            expect(Calendar.week_number_of_year(Time.new(2011, 2, 8))).to eql(6)
+            expect(Calendar.week_number_of_year(Time.new(2012, 4, 23))).to eql(17)
+            expect(Calendar.week_number_of_year(Time.new(2015, 7, 31))).to eql(31)
+            expect(Calendar.week_number_of_year(Time.new(2014, 12, 7))).to eql(49)
+        end
     end
 
-    it "responds to instance method in_year" do
-        expect(Calendar.for_week(2)).to respond_to(:in_year)
+    context ".current_week_number_of_current_year" do
+        it "returns the ongoing week-number of the current year" do
+            expect(Calendar.current_week_number_of_current_year).to eql(current_week_number_of_current_year)
+        end
+    end
+
+    context ".current_week_number_of_current_month" do
+        it "returns the ongoing week-number of the current month" do
+            expect(Calendar.current_week_number_of_current_month).to eql(current_week_number_of_current_month)
+        end
     end
 
     context ".for_year" do
@@ -58,10 +126,6 @@ describe Calendar do
     context ".for_month.in_year" do
         let!(:calendar) { Calendar.for_month(2).in_year(2012) }
 
-        it "responds to instance method in_year" do
-            expect(calendar).to respond_to(:in_year)
-        end
-
         it "sets the year to received year" do
             expect(calendar.year).to eql(2012)
         end
@@ -77,14 +141,6 @@ describe Calendar do
 
     context ".for_week.of_month.in_year" do
         let!(:calendar) { Calendar.for_week(2).of_month(4).in_year(2014) }
-
-        it "responds to instance method of_month" do
-            expect(calendar).to respond_to(:of_month)
-        end
-
-        it "responds to instance method in_year" do
-            expect(calendar).to respond_to(:in_year)
-        end
 
         it "sets the year to received year" do
             expect(calendar.year).to eql(2014)
@@ -102,10 +158,6 @@ describe Calendar do
     context ".of_month" do
         let!(:calendar) { Calendar.of_month(8) }
 
-        it "responds to class method of_month" do
-            expect(Calendar).to respond_to(:of_month)
-        end
-
         it "sets the month to received month" do
             expect(calendar.month).to eql(8)
         end
@@ -118,10 +170,6 @@ describe Calendar do
     context ".for_current_week" do
         let!(:calendar) { Calendar.for_current_week }
 
-        it "it responds to class method for_current_week" do
-            expect(Calendar).to respond_to(:for_current_week)
-        end
-
         it "sets the year to current year" do
             expect(calendar.year).to eql(Time.now.year)
         end
@@ -130,17 +178,13 @@ describe Calendar do
             expect(calendar.month).to eql(Time.now.month)
         end
 
-        it "sets the week to current week number in year" do
-            expect(calendar.week).to eql(Time.now.strftime("%W").to_i)
+        it "sets the week to current week-number of year" do
+            expect(calendar.week).to eql(current_week_number_of_current_year)
         end
     end
 
     context ".for_current_month" do
         let!(:calendar) { Calendar.for_current_month }
-
-        it "it responds to class method for_current_month" do
-            expect(Calendar).to respond_to(:for_current_month)
-        end
 
         it "sets the year to current year" do
             expect(calendar.year).to eql(Time.now.year)
@@ -157,10 +201,6 @@ describe Calendar do
 
     context ".for_current_year" do
         let!(:calendar) { Calendar.for_current_year }
-
-        it "it responds to class method for_current_year" do
-            expect(Calendar).to respond_to(:for_current_year)
-        end
 
         it "sets the year to current year" do
             expect(calendar.year).to eql(Time.now.year)
@@ -193,6 +233,38 @@ describe Calendar do
         end
 
         # TODO: Add more specs
+
+        # TODO: What if the week number passed is invalid in context of of_month value
+        # For e.g. for_week(7).of_month(6)
+        # There cannot be 7 weeks in a month
+
+        # TODO: What if the month number passed is invalid in context of in_year value
+        # For e.g. for_month(13).in_year(2012)
+        # There cannot be 13th month in a year
+        # Similarly there cannot be 0th or negative month in a year
+
+        # http://stackoverflow.com/questions/1489826/how-to-get-the-number-of-days-in-a-given-month-in-ruby-accounting-for-year
+        # https://github.com/sachin87/week-of-month/blob/master/lib/modules/day.rb
+        # http://en.wikipedia.org/wiki/ISO_week_date#Dates_with_fixed_week_number
     end
 
+    # SPEC HELPER METHODS
+    def current_week_number_of_current_year
+        Time.now.strftime("%W").to_i
+    end
+
+    def current_week_number_of_current_month
+        current_date = Time.now.day
+        if current_date <= 7
+            1
+        elsif current_date.between?(8, 14)
+            2
+        elsif current_date.between?(15, 21)
+            3
+        elsif current_date.between?(22, 28)
+            4
+        else
+            5
+        end
+    end
 end
